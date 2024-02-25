6dbad85b71234a5783e2e34270a6e9d4e6db26e1
Omkar Patel <87596092+incpi@users.noreply.github.com>
2024-02-26 00:39:22 +0530

    Correction to mimic properly

--- test_veeva_server.js
+++ test_veeva_server.js
@@ -8,17 +8,21 @@ app.use(bodyParser.json());
 const Authorizations = new Map();
 app.post("/auth", bodyParser.urlencoded({ extended: true }), (req, res) => {
   const { username, password } = req.body;
- console.log(req.headers)
+  console.log(req.headers);
   const contenttype = req.headers["accept"];
   if (username === "a" && password === "a") {
     const authorization =
       Math.random().toString(36).substring(2, 15) +
       Math.random().toString(36).substring(2, 15);
     if (contenttype.toLowerCase() === "application/xml") {
-      `<response><status>success</status><authorization>${authorization}</authorization></response>`;
+      `<response><responseStatus>SUCCESS</responseStatus><sessionId>${authorization}</sessionId><userId>12021</userId></response>`;
     } else {
       Authorizations.set(authorization, true);
-      res.json({ authorization });
+      res.json({
+        responseStatus: "SUCCESS",
+        sessionId: authorization,
+        userId: 12021,
+      });
     }
     console.log(Authorizations);
   } else {
@@ -26,19 +30,19 @@ app.post("/auth", bodyParser.urlencoded({ extended: true }), (req, res) => {
   }
 });
 
-app.post("/veeva/query", (req, res) => {
+app.post("/query", (req, res) => {
   console.log(req.headers);
-  const Authorization = req.headers["authorization"];
-   const contenttype = req.headers["accept"];
+  const Authorization = req.headers["Authorization".toLowerCase()];
+  const contenttype = req.headers["Accept".toLowerCase()];
 
   if (Authorizations.has(Authorization)) {
     const documentId = req.query.q || "";
     if (contenttype.toLowerCase() === "application/xml") {
-      const retrievedDocument = `<response><status>success</status><data><record><id>${documentId}</id><name>Product A</name><description>This is a description of Product A</description></record></data></response>`;
+      const retrievedDocument = `<response><responseStatus>SUCCESS</responseStatus><data><record><id>${documentId}</id><name>Product A</name><description>This is a description of Product A</description></record></data></response>`;
       res.status(200).send(retrievedDocument);
     } else {
       const retrievedDocument = {
-        status: "success",
+         responseStatus: "SUCCESS",
         data: [
           {
             id: documentId,
@@ -50,7 +54,7 @@ app.post("/veeva/query", (req, res) => {
       res.status(200).json(retrievedDocument);
     }
   } else {
-    res.status(401).json({ error: "Unauthorized" });
+    res.status(401).json({ responseStatus: "Failure" });
   }
 });
 
@@ -61,16 +65,18 @@ const server = app.listen(port, () => {
 const request = supertest(server);
 request
   .post("/auth")
-  .send("username=a&password=a").set("Accept", "application/json")
+  .send("username=a&password=a")
+  .set("Accept", "application/json")
   .expect(200)
   .end((err, res) => {
     if (err) {
       console.error(err);
       return;
     }
-    const Authorization = res.body.authorization;
+    const Authorization = res.body.sessionId;
+   
     request
-      .post("/veeva/query?q=1_json")
+      .post("/query?q=SELECT id FROM documents")
       .set("Authorization", Authorization)
       .set("Accept", "application/json")
       .expect(200)
@@ -85,7 +91,7 @@ request
 
     // Make XML document retrieval request
     request
-      .post("/veeva/query?q=1_xml")
+      .post("/query?q=1_xml")
       .set("Authorization", Authorization)
       .set("Accept", "application/xml")
       .expect(200)
